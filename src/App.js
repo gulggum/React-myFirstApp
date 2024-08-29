b180f92341b5d7f2abe704e62cb062389777186a
gulggum <163222604+gulggum@users.noreply.github.com>
2024-08-29 13:35:54 +0900

    TodoList_practice
    todoList_practice
    1. map함수이용(컴포넌트에 return)
    2. filter 함수이용(삭제기능)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState } from "react";
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setTodo("");
+    setTodoList((newTodo) => [todo, ...newTodo]);
+    console.log(todoList);
+  };
+  const onDelete = (index) => {
+    setTodoList(todoList.filter((_, todoIndex) => index !== todoIndex)); //index 는 배열숫자,즉 현재 몇번째인지 위치한 번호(0,1,2..)
+  };
+
   return (
     <div>
-      <h1>Welcome Back!!!!</h1>
+      <h1>My Todo List</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          onChange={onChange}
+          type="text"
+          placeholder="Please your Todo"
+        ></input>
+        <button>Submit</button>
+      </form>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => onDelete(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
